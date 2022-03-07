e6b5a6976e397e7704bea8dde33c639f87ff6dbc
divyasrvn <91505218+divyasrvn@users.noreply.github.com>
2022-03-07 13:12:53 -0500

    Create main.tf

---
+++ modules/main.tf
@@ -0,0 +1,33 @@
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
+}
+
+resource "google_compute_instance" "gce-vm" {
+  name          = var.instance_name
+  machine_type  = var.gce_machine_type
+  zone          = var.gce_zone
+  labels        = var.gce_labels
+  tags          = var.gce_tags
+  project       = var.project_id 
+  boot_disk {
+    initialize_params {
+      image = "rhel-cloud/rhel-7"
+      type  = "pd-ssd"
+      size  = var.disk_size //change here to 100
+    }
+  }
+
+  // Local SSD disk
+  //scratch_disk {
+  //  interface = "SCSI"
+  //}
+
+  network_interface {
+    network = var.vpc_network
+    access_config {
+      nat_ip = google_compute_address.static.address
+    }
+  }
+
+  metadata_startup_script = var.startup_scripts
+}
