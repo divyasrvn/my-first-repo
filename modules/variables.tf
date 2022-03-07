21ec0953fbee04f06325c0d0233a9b57a0f42b6c
divyasrvn <91505218+divyasrvn@users.noreply.github.com>
2022-03-07 13:13:26 -0500

    Create variables.tf

---
+++ modules/variables.tf
@@ -0,0 +1,40 @@
+variable "instance_name" {
+    description = "Name of GCE Instance"
+    type = string  
+}
+
+variable "gce_machine_type" {
+  type        = string
+  description = "GCE machine_type"
+}
+variable "gce_zone" {
+  type        = string
+  description = "GCE zone to create VM in"
+}
+variable "gce_labels" {
+  type        = string
+  description = "optional labels to attch to VM"
+}
+variable "gce_tags" {
+  type        = string
+  description = "gce_tags to attch to VM"
+}
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+}
+variable "disk_size" {
+  type        = string
+  default = "100"
+  description = "GCE boot disk size"
+}
+variable "startup_scripts" {
+  type        = string
+  default       = "echo hi > /test.txt"
+  description = "Startup script either file or commands"
+} 
+variable "vpc_network" {
+  type        = string
+  default       = "default"
+  description = "VPC network to host GCE VMS intos"
+}
