34d5ba79890271752c1a45e69f23a0833b25a71e
divyasrvn <91505218+divyasrvn@users.noreply.github.com>
2022-03-07 13:09:28 -0500

    Create variable.tf

---
+++ nessus/variable.tf
@@ -0,0 +1,13 @@
+variable "gce_vm_details" {
+  type = map(object({
+    instance_name                   = string
+    gce_machine_type                = string
+    gce_zone                        = string
+    gce_labels                      = map(string)
+    gce_tags                        = list(string)
+    project_id                      = string
+    disk_size                       = string
+    vpc_network                     = string
+    startup_scripts                 = string
+  }))
+}
